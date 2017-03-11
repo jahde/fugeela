457aeea6990a25528e9129bbf2c4dc5e34bc1071
jahde <jahde@classpass.com>
2017-03-11 13:40:37 -0500

    added video route

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+// Authenticator
+app.use(express.basicAuth('testUser', 'testPass'));
+
 app.get('/', function (req, res) {
   // res.send('Hello World Jahde!')
   res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/jahde', function (req, res) {
+  // res.send('Hello World Jahde!')
+  res.sendFile(path.join(__dirname + '/video.html'))
 })
 
 app.listen(3000, function () {
